3a83b7888ae5ff086f1a926e0068a16c355ae09b
kingsarip8@gmail.com <kingsarip8@gmail.com>
2026-05-04 22:20:33 +0800

    final project submission

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
-const supabaseUrl = 'https://brzhpcgbmjcdltyojyyp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyemhwY2dibWpjZGx0eW9qeXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MDE1MTQsImV4cCI6MjA5MzE3NzUxNH0.HKvqQdhROm4A4Qh-c2dXgAgVCtnoJSDF6mQkcP86fTM';
-
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: Platform.OS === 'web' ? undefined : AsyncStorage,
